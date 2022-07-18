bcf27a76d73406edefd20b6fb7c513b2d3f32b61
Andrew Smith <meuzishun@gmail.com>
2022-07-18 16:57:41 -0400

    Start bringing in firebase
    Install firebase from npm, add necessary code for main js file.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -6,6 +10,18 @@ import Game from './components/Game';
 import NewGame from './components/NewGame';
 import HighScores from './components/HighScores';
 import Footer from './components/Footer';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCkDUn5Z6gvSm-r9yb4UbKNBMxfAlOyUrk',
+  authDomain: 'whereswaldo-ea81f.firebaseapp.com',
+  projectId: 'whereswaldo-ea81f',
+  storageBucket: 'whereswaldo-ea81f.appspot.com',
+  messagingSenderId: '462284750954',
+  appId: '1:462284750954:web:256577b4667fcb2b190a13',
+};
+// Initialize Firebase
+const backendApp = initializeApp(firebaseConfig);
 
 function App() {
   const [gamePic, setGamePic] = useState(undefined);
